675e10df3e7087c02b62201b93835084ea5ddf08
Joe Zlonicky <joe.zlonicky@gmail.com>
2023-11-26 14:25:02 -0800

    Clean up HTML

--- script.js
+++ script.js
@@ -8,47 +8,47 @@ const miscFilter = {};
 let cards = [];
 
 async function fetchFissureData() {
-    const response = await fetch(API_URL);
-    const fissures = await response.json();
+    // const response = await fetch(API_URL);
+    // const fissures = await response.json();
 
-    // const fissures = [{
-    //     "id": "6559332eb9b14087f8219611",
-    //     "activation": "2023-11-18T21:57:02.110Z",
-    //     "startString": "-1h 2m 42s",
-    //     "expiry": "2023-11-18T23:55:53.181Z",
-    //     "active": true,
-    //     "node": "Olympus (Mars)",
-    //     "missionType": "Disruption",
-    //     "missionKey": "Disruption",
-    //     "enemy": "Grineer",
-    //     "enemyKey": "Grineer",
-    //     "nodeKey": "Olympus (Mars)",
-    //     "tier": "Lith",
-    //     "tierNum": 1,
-    //     "expired": false,
-    //     "eta": "56m 8s",
-    //     "isStorm": false,
-    //     "isHard": false
-    // },
-    // {
-    //     "id": "65593585fd63d3abfa3b6101",
-    //     "activation": "2023-11-18T22:07:01.678Z",
-    //     "startString": "-52m 42s",
-    //     "expiry": "2023-11-18T23:18:36.715Z",
-    //     "active": true,
-    //     "node": "Umbriel (Uranus)",
-    //     "missionType": "Interception",
-    //     "missionKey": "Interception",
-    //     "enemy": "Grineer",
-    //     "enemyKey": "Grineer",
-    //     "nodeKey": "Umbriel (Uranus)",
-    //     "tier": "Neo",
-    //     "tierNum": 3,
-    //     "expired": false,
-    //     "eta": "18m 52s",
-    //     "isStorm": false,
-    //     "isHard": false
-    // }];
+    const fissures = [{
+        "id": "6559332eb9b14087f8219611",
+        "activation": "2023-11-18T21:57:02.110Z",
+        "startString": "-1h 2m 42s",
+        "expiry": "2023-11-18T23:55:53.181Z",
+        "active": true,
+        "node": "Olympus (Mars)",
+        "missionType": "Disruption",
+        "missionKey": "Disruption",
+        "enemy": "Grineer",
+        "enemyKey": "Grineer",
+        "nodeKey": "Olympus (Mars)",
+        "tier": "Lith",
+        "tierNum": 1,
+        "expired": false,
+        "eta": "56m 8s",
+        "isStorm": false,
+        "isHard": false
+    },
+    {
+        "id": "65593585fd63d3abfa3b6101",
+        "activation": "2023-11-18T22:07:01.678Z",
+        "startString": "-52m 42s",
+        "expiry": "2023-11-18T23:18:36.715Z",
+        "active": true,
+        "node": "Umbriel (Uranus)",
+        "missionType": "Interception",
+        "missionKey": "Interception",
+        "enemy": "Grineer",
+        "enemyKey": "Grineer",
+        "nodeKey": "Umbriel (Uranus)",
+        "tier": "Neo",
+        "tierNum": 3,
+        "expired": false,
+        "eta": "18m 52s",
+        "isStorm": false,
+        "isHard": false
+    }];
     return fissures;
 }
 
@@ -117,8 +117,8 @@ function removeAllCards() {
 
 function setupFilterButtons(fieldsetSelector, filter) {
     const fieldset = document.querySelector(fieldsetSelector);
-    const allButton = fieldset.querySelector('.all-filter-button');
-    const noneButton = fieldset.querySelector('.none-filter-button');
+    const allButton = fieldset.querySelector('.filter-form__all-button');
+    const noneButton = fieldset.querySelector('.filter-form__none-button');
     const checkboxes = fieldset.querySelectorAll('input');
 
     if (allButton) {
@@ -162,10 +162,10 @@ async function populateCards() {
 }
 
 populateCards().then(() => {
-    setupFilterButtons('.tier-fieldset', tierFilter);
-    setupFilterButtons('.mission-type-fieldset', missionTypeFilter);
-    setupFilterButtons('.other-mission-types-fieldset', missionTypeFilter);
-    setupFilterButtons('.misc-fieldset', miscFilter);
+    setupFilterButtons('.filter-form__tier', tierFilter);
+    setupFilterButtons('.filter-form__mission-type', missionTypeFilter);
+    setupFilterButtons('.filter-form__other-mission-type', missionTypeFilter);
+    setupFilterButtons('.filter-form__misc', miscFilter);
     updateCards();
 
     setInterval(() => {
